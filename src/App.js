0960e6500c86f50a571bfb922f3196b81a155490
Morton358 <vovankovalchuk358@gmail.com>
2018-03-20 00:58:11 +0100

    add react-transition in project and created animation of showing modal with mounting and unmounting it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -7,7 +8,8 @@ import List from './components/List/List';
 
 class App extends Component {
     state = {
-        modalIsOpen: false
+        modalIsOpen: false,
+        redBlock: false
     };
     handleOpenModal = () => {
         this.setState({ modalIsOpen: true });
@@ -15,15 +17,44 @@ class App extends Component {
     handleCloseModal = () => {
         this.setState({ modalIsOpen: false });
     };
+    handleOpenRedBlock = () => {
+        this.setState(prevState => ({ redBlock: !prevState.redBlock }));
+    };
     render() {
         return (
             <div className="App">
                 <h1>React Animations</h1>
+                <button className="Button" onClick={this.handleOpenRedBlock}>
+                    Toggle
+                </button>
+                <br />
+                <Transition
+                    in={this.state.redBlock}
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    {state => (
+                        <div
+                            style={{
+                                backgroundColor: 'red',
+                                margin: 'auto',
+                                height: 80,
+                                width: 80,
+                                transition: 'opacity 1s ease-out',
+                                opacity: state === 'exiting' ? 0 : 1
+                            }}
+                        >
+                            <p>{state}</p>
+                        </div>
+                    )}
+                </Transition>
                 <Modal
                     show={this.state.modalIsOpen}
                     closed={this.handleCloseModal}
                 />
-                <Backdrop show={this.state.modalIsOpen} />
+                {this.state.modalIsOpen ? <Backdrop show /> : null}
+                <br />
                 <button className="Button" onClick={this.handleOpenModal}>
                     Open Modal
                 </button>
