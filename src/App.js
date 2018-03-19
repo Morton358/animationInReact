48d29fb46609ce595cfe5fd2bab77bc516234d6b
Morton358 <vovankovalchuk358@gmail.com>
2018-03-19 18:30:34 +0100

    add css animation for open and closing modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
-import Modal from "./components/Modal/Modal";
-import Backdrop from "./components/Backdrop/Backdrop";
-import List from "./components/List/List";
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+    state = {
+        modalIsOpen: false
+    };
+    handleOpenModal = () => {
+        this.setState({ modalIsOpen: true });
+    };
+    handleCloseModal = () => {
+        this.setState({ modalIsOpen: false });
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1>React Animations</h1>
+                <Modal
+                    show={this.state.modalIsOpen}
+                    closed={this.handleCloseModal}
+                />
+                <Backdrop show={this.state.modalIsOpen} />
+                <button className="Button" onClick={this.handleOpenModal}>
+                    Open Modal
+                </button>
+                <h3>Animating Lists</h3>
+                <List />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,39 +5,44 @@ import './List.css';
 class List extends Component {
     state = {
         items: [1, 2, 3]
-    }
+    };
 
     addItemHandler = () => {
-        this.setState((prevState) => {
+        this.setState(prevState => {
             return {
                 items: prevState.items.concat(prevState.items.length + 1)
             };
         });
-    }
+    };
 
-    removeItemHandler = (selIndex) => {
-        this.setState((prevState) => {
+    removeItemHandler = selIndex => {
+        this.setState(prevState => {
             return {
-                items: prevState.items.filter((item, index) => index !== selIndex)
+                items: prevState.items.filter(
+                    (item, index) => index !== selIndex
+                )
             };
         });
-    }
+    };
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
+    render() {
+        const listItems = this.state.items.map((item, index) => (
+            <li
                 key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+                className="ListItem"
+                onClick={() => this.removeItemHandler(index)}
+            >
+                {item}
+            </li>
+        ));
 
         return (
             <div>
-                <button className="Button" onClick={this.addItemHandler}>Add Item</button>
+                <button className="Button" onClick={this.addItemHandler}>
+                    Add Item
+                </button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
-                    {listItems}
-                </ul>
+                <ul className="List">{listItems}</ul>
             </div>
         );
     }
